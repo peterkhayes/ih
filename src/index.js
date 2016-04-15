5d22e151bfa36748fc3bfb9b5298cb92171505af
Peter Hayes <peter.k.hayes@gmail.com>
2016-04-14 22:47:00 -0700

    fixed bugs in inc and merge

--- src/index.js
+++ src/index.js
@@ -55,7 +55,7 @@ function without (obj, path) {
       delete newObj[headKey];
       return newObj;
     } else {
-      return { ...obj, [headKey]: without(obj, tailKeys) };
+      return { ...obj, [headKey]: without(obj[headKey], tailKeys) };
     }
   }
 }
@@ -73,7 +73,7 @@ function transformDeep (obj, path, fn) {
 
 function merge (obj, path, toMerge) {
   if (arguments.length === 2) {
-    return mergeOp(obj, path);
+    return mergeOp(path, obj);
   } else {
     return transform(obj, path, mergeOp.bind(null, toMerge));
   }
@@ -161,7 +161,20 @@ function setOp (obj, path, val, deep) {
 }
 
 function mergeOp (toMerge, existing = {}) {
-  return {...existing, ...toMerge};
+  const isArray = Array.isArray(toMerge);
+  if (typeof toMerge !== "object") {
+    throw new Error(`Can't merge a primitive: ${toMerge}`);
+  } else if (typeof existing !== "object") {
+    throw new Error(`Can't merge a primitive: ${existing}`);
+  } else if (isArray !== Array.isArray(existing)) {
+    throw new Error("Can't merge an array onto a non-array.");
+  } else if (!isArray) {
+    return {...existing, ...toMerge};
+  } else if (toMerge.length > existing.length) {
+    return toMerge;
+  } else {
+    return toMerge.concat(existing.slice(toMerge.length));
+  }
 }
 
 function toggleOp (val) {
@@ -169,7 +182,7 @@ function toggleOp (val) {
 }
 
 function incOp (amt = 1, existing) {
-  if (typeof amount !== "number") {
+  if (typeof amt !== "number") {
     throw new Error(`Attempted to increment by a non-numerical value: ${amt}`);
   } else if (existing == null) {
     return amt;
--- test/index.js
+++ test/index.js
@@ -76,12 +76,12 @@ describe("ih", () => {
 
     const val = {foo: "bar"};
 
-    it("sets a primative key to a root-level path", () => {
+    it("sets a primitive key to a root-level path", () => {
       copy.z = val;
       expect(ih.set(obj, "z", val)).toEqual(copy);
     });
 
-    it("overwrites a primative key to a root-level path", () => {
+    it("overwrites a primitive key to a root-level path", () => {
       copy.a = val;
       expect(ih.set(obj, "a", val)).toEqual(copy);
     });
@@ -130,12 +130,12 @@ describe("ih", () => {
 
     const val = {foo: "bar"};
 
-    it("sets a primative key to a root-level path", () => {
+    it("sets a primitive key to a root-level path", () => {
       copy.z = val;
       expect(ih.setDeep(obj, "z", val)).toEqual(copy);
     });
 
-    it("overwrites a primative key to a root-level path", () => {
+    it("overwrites a primitive key to a root-level path", () => {
       copy.a = val;
       expect(ih.setDeep(obj, "a", val)).toEqual(copy);
     });
@@ -187,23 +187,98 @@ describe("ih", () => {
 
   });
 
-  // describe("inc", () => {
+  describe("without", () => {
 
-  //   it("increments a value by provided amount", () => {
-  //     copy.c.d += 5;
-  //     expect(ih.inc(obj, "c.d", 5)).toEqual(copy);
-  //   });
+    it("removes a root-level value", () => {
+      delete copy.a;
+      expect(ih.without(obj, "a")).toEqual(copy);
+    });
+
+    it("removes a nested value", () => {
+      delete copy.c.e;
+      expect(ih.without(obj, "c.e")).toEqual(copy);
+    });
+
+    it("removes an array value", () => {
+      copy.arr.splice(1, 1);
+      expect(ih.without(obj, "arr.1")).toEqual(copy);
+    });
+
+    it("does nothing for non-existant values", () => {
+      expect(ih.without(obj, "c.e.d")).toEqual(copy);
+    });
+
+  });
+
+  describe("merge", () => {
+
+    it("merges two objects at the given path", () => {
+      copy.c.e = 5;
+      copy.c.f = 6;
+      expect(ih.merge(obj, "c", {e: 5, f: 6})).toEqual(copy);
+    });
     
-  //   it("increments a value by 1 if no amount is provided", () => {
-  //     copy.c.e.f += 1;
-  //     expect(ih.inc(obj, "c.e.f")).toEqual(copy);
-  //   });
+    it("merges a shorter array at the given path", () => {
+      copy.arr[0] = 5;
+      expect(ih.merge(obj, "arr", [5])).toEqual(copy);
+    });
 
-  //   it("sets a non-existant value", () => {
-  //     copy.c.e.
-  //   });
+    it("merges a longer array at the given path", () => {
+      copy.arr = [1, 2, 3, 4, 5];
+      expect(ih.merge(obj, "arr", [1, 2, 3, 4, 5])).toEqual(copy);
+    });
 
+    it("merges two objects at the root", () => {
+      copy.a = 2;
+      copy.b = 3;
+      expect(ih.merge(obj, {a: 2, b: 3})).toEqual(copy);
+    });
 
-  // });
+    it("merges a shorter array at the root", () => {
+      copy.arr[0] = 5;
+      expect(ih.merge(obj.arr, [5])).toEqual(copy.arr);
+    });
+
+    it("merges a longer array at the root", () => {
+      copy.arr = [1, 2, 3, 4, 5];
+      expect(ih.merge(obj.arr, [1, 2, 3, 4, 5])).toEqual(copy.arr);
+    });
+
+    it("throws if an array is merged with an object", () => {
+      expect(() => ih.merge(obj, "arr", {})).toThrow();
+    });
+
+    it("throws if an object is merged with an array", () => {
+      expect(() => ih.merge(obj, "c", [])).toThrow();
+    });
+
+    it("throws if a primitive is merged with an object", () => {
+      expect(() => ih.merge(obj, "a", {})).toThrow();
+    });
+
+    it("throws if an object is merged with a primitive", () => {
+      expect(() => ih.merge(obj, "c", 1)).toThrow();
+    });
+
+  });
+
+  describe("inc", () => {
+
+    it("increments a value by provided amount", () => {
+      copy.c.d -= 5;
+      expect(ih.inc(obj, "c.d", -5)).toEqual(copy);
+    });
+    
+    it("increments a value by 1 if no amount is provided", () => {
+      copy.c.e.f += 1;
+      expect(ih.inc(obj, "c.e.f")).toEqual(copy);
+    });
+
+    it("sets a non-existant value", () => {
+      copy.c.e.h = 2;
+      expect(ih.inc(obj, "c.e.h", 2)).toEqual(copy);
+    });
+
+  });
 
 });
